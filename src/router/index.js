2aaee858a2bdccce477fde7a2a19dc686e31b51b
Andrew Gomes <andrewgomes1328@gmail.com>
2024-02-05 20:45:30 -0300

    feat: funcionalidade para ver o time mais comum finalizado

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import NotFound from "../views/NotFound.vue";
+import TimeMaisComum from "../views/TimeMaisComum.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: "notfound",
       component: NotFound,
     },
+    {
+      path: "/timemaiscomum",
+      name: "timemaiscomum",
+      component: TimeMaisComum
+    }
   ],
 });
 
---
+++ src/views/TimeMaisComum.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Resultado />
+    </div>
+</template>
+
+<script>
+import Resultado from '../components/TimeMaisComum/Resultado.vue';
+
+export default {
+    name: "TimeMaisComum",
+    components: {
+        Resultado
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
